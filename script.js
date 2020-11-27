5595a2c343de3664fcfc5c3c4e892c77526b889e
Dmitri So <dmitriso@yahoo.com>
2020-11-27 14:32:28 -0500

    Set conditions for password criteria selection.

--- script.js
+++ script.js
@@ -9,16 +9,13 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
 // Need variables to store all password criteria uppercase, lowercase, numbers, and special characters.
-var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", ":", ";", "'", "\"", "<", ">", ",", ".", "\/", "\\", "|", "?"];
+var allUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var allLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var allNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var allSpecial = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", ":", ";", "'", "\"", "<", ">", ",", ".", "\/", "\\", "|", "?"];
 // Prompt user to select password criteria
 // Asks user to select amount of characters between 8-128
 // Then ask if they want to use Uppercase letters
@@ -34,33 +31,51 @@ function generatePassword() {
   var newPasssword = "";
   // All user selected criteria Uppercase letters, Lowercase letters, Numbers, Special characters
   var allChoice = [];
+  // Prompt user for new password length
   var passwordLength = prompt("How many characters would you like to use? (Must be between 8-128 characters)");
   // Conditions to select new password criteria.
-  // Password length user selection
-  if (passwordLength >=8 && passwordLength <= 128) {
+  // Password length user selection condition
+  if (passwordLength >= 8 && passwordLength <= 128) {
     console.log(passwordLength);
     // UpperCase?
-    var useUpper= confirm("Do you want uppercase letters?");
+    var useUpper = confirm("Do you want uppercase letters?");
     console.log(useUpper);
     // LowerCase?
-    var useLower= confirm("Do you want to use lowercase letters?");
+    var useLower = confirm("Do you want to use lowercase letters?");
     console.log(useLower);
     // Numbers?
-    var useNumber= confirm("Do you want to use numbers?");
+    var useNumber = confirm("Do you want to use numbers?");
     console.log(useNumber);
     // SpecialCharacters?
-    var useSpecial= confirm("Do you want to use special characters?");
+    var useSpecial = confirm("Do you want to use special characters?");
     console.log(useSpecial);
-  // Password criteria selection Uppercase letters, Lowercase letters, Numbers, Special characters
+    // Setting password criteria conditions (Uppercase letters, Lowercase letters, Numbers, Special characters)
+    // If user selects uppercase. Characters are combined with allChoice array
+    if (useUpper === true) {
+      allChoice.concat(allUpper);
+    }
+    // If user selects lowercase. Characters are combined with allChoice array
+    if (useLower === true) {
+      allChoice.concat(useLower);
+    }
+    // If use selects numbers. Characters are combined with allChoice array
+    if (useNumber === true) {
+      allChoice.concat(useNumber);
+    }
+    // If user selects special characters. Characters are combined with allChoice array
+    if (useSpecial === true) {
+      allChoice.concat(useSpecial);
+    }
 
-  
+
+
 
 
     return newPasssword;
   } else {
     console.log(passwordLength);
-    alert ("Error! Must contain between 8-128 characters.");
-    return("Try again!");
+    alert("Error! Must contain between 8-128 characters.");
+    return ("Try again!");
 
 
   }
