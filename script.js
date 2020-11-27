31f5e3e77a65eff081803b152d86e52dea59ec5f
Dmitri So <dmitriso@yahoo.com>
2020-11-27 13:38:16 -0500

    Created variables for arrays containing Uppercase letters, Lowercase letters, Numbers, and Special characters.

--- script.js
+++ script.js
@@ -14,7 +14,11 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
-// Need variables to store all password criteria uppercase, lowercase, numbers, and special characters
+// Need variables to store all password criteria uppercase, lowercase, numbers, and special characters.
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", ":", ";", "'", "\"", "<", ">", ",", ".", "\/", "\\", "|", "?"];
 // Prompt user to select password criteria
 // Asks user to select amount of characters between 8-128
 // Then ask if they want to use Uppercase letters
@@ -22,6 +26,5 @@ generateBtn.addEventListener("click", writePassword);
 // Ask user if they want to use Numbers
 // Ask user if they want to use Special characters
 // Based off selections randomly generates new password
-// Once newpassword is generated return result to user
-
+// Once new password is generated return result to user
 
