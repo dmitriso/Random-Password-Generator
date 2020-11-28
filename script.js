a6efdb0f73c0124c12bb2c878452922c4a10e2c9
Dmitri So <dmitriso@yahoo.com>
2020-11-28 18:16:11 -0500

    Creates randomly generated password and returns result to user

--- script.js
+++ script.js
@@ -11,77 +11,83 @@ function writePassword() {
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-// Need variables to store all password criteria uppercase, lowercase, numbers, and special characters.
+
+
+// Randomly Generates the password here
+function generatePassword() {
+// Variables to store all password criteria uppercase, lowercase, numbers, and special characters.
 var allUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var allLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var allNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var allSpecial = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", ":", ";", "'", "\"", "<", ">", ",", ".", "\/", "\\", "|", "?"];
-// Prompt user to select password criteria
-// Asks user to select amount of characters between 8-128
-// Then ask if they want to use Uppercase letters
-// Ask user if they want to use Lowercase letters
-// Ask user if they want to use Numbers
-// Ask user if they want to use Special characters
-// Based off selections randomly generates new password
-// Once new password is generated return result to user
 
-// Randomly Generates the password here
-function generatePassword() {
   // Stores results for new randomly generated password
   var newPasssword = "";
+
   // All user selected criteria Uppercase letters, Lowercase letters, Numbers, Special characters
   var allChoice = [];
+
   // Prompt user for new password length
-  var passwordLength = prompt("How many characters would you like to use? (Must be between 8-128 characters)");
+  var passwordLength = parseInt(prompt("How many characters would you like to use? (Must be between 8-128 characters)"));
+
   // Conditions to select new password criteria.
   // Password length user selection condition
   if (passwordLength >= 8 && passwordLength <= 128) {
-    console.log(passwordLength);
+console.log(passwordLength);
+
     // UpperCase?
     var useUpper = confirm("Do you want uppercase letters?");
-    console.log(useUpper);
+console.log(useUpper);
+
     // LowerCase?
     var useLower = confirm("Do you want to use lowercase letters?");
-    console.log(useLower);
+console.log(useLower);
+
     // Numbers?
     var useNumber = confirm("Do you want to use numbers?");
-    console.log(useNumber);
+console.log(useNumber);
+
     // SpecialCharacters?
     var useSpecial = confirm("Do you want to use special characters?");
-    console.log(useSpecial);
+console.log(useSpecial);
+
     // Setting password criteria conditions (Uppercase letters, Lowercase letters, Numbers, Special characters)
     // If user selects uppercase. Characters are combined with allChoice array
     if (useUpper === true) {
-      allChoice.concat(allUpper);
+      allChoice = allChoice.concat(allUpper);
+      console.log(allChoice);
     }
+
     // If user selects lowercase. Characters are combined with allChoice array
     if (useLower === true) {
-      allChoice.concat(useLower);
+      allChoice = allChoice.concat(allLower);
+      console.log(allChoice);
     }
+
     // If use selects numbers. Characters are combined with allChoice array
     if (useNumber === true) {
-      allChoice.concat(useNumber);
+      allChoice = allChoice.concat(allNumber);
+      console.log(allChoice);
     }
+
     // If user selects special characters. Characters are combined with allChoice array
     if (useSpecial === true) {
-      allChoice.concat(useSpecial);
+      allChoice = allChoice.concat(allSpecial);
+      console.log(allChoice);
     }
 
-
-
-
-
-    return newPasssword;
+    
+    // Randomly generate new password based off user selection
+    for (var i = 0; i < passwordLength; i++) {
+      var randomIndex = Math.floor(Math.random() * allChoice.length);
+      console.log(randomIndex);
+      // Sets results of random index to the users new password
+      newPasssword += allChoice[randomIndex];
+    }
   } else {
-    console.log(passwordLength);
     alert("Error! Must contain between 8-128 characters.");
     return ("Try again!");
-
-
   }
-
-
-
   // Once newpassword is generated return result to user
   return newPasssword;
 }
